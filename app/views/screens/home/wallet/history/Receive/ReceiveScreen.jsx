ed04e26a23f829c36c355df21af44b0fe149b6fd
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2018-09-08 23:33:10 +0200

    balance added

--- app/views/screens/home/wallet/history/Receive/ReceiveScreen.jsx
+++ app/views/screens/home/wallet/history/Receive/ReceiveScreen.jsx
@@ -9,17 +9,28 @@ import {
     TouchableOpacity
 } from 'react-native';
 import QRCode from 'react-native-qrcode';
-import { getActiveWallet } from 'quid-wallet/app/data/selectors';
+
 import TransparentNavBar from 'quid-wallet/app/views/components/TransparentNavBar';
 import styles from './styles';
 import { shortAddress } from 'quid-wallet/app/utils';
+import {
+    getActiveWalletTotalBalance,
+    getActiveWallet,
+    getSelectedCurrency
+} from 'quid-wallet/app/data/selectors';
+import { formatToCurrency } from 'quid-wallet/app/utils';
 
 
-class ReceiveScreen extends React.Component {
+
+class ReceiveScreen extends React.Component {    
     static navigatorStyle = {
 	statusBarTextColorSchemeSingleScreen: 'light',	
 	navBarHidden: true,	
 	screenBackgroundColor: '#fff'
+    }
+
+    state = {
+	balanceHidden: true
     }
     
     async _fetchData() {	
@@ -56,6 +67,46 @@ class ReceiveScreen extends React.Component {
 	    </View>
 	)
     }
+
+    _renderInfo() {
+	return (
+	    <View style={{backgroundColor: '#B1F1B8'}}>
+	      <Text style={{fontSize: 18, padding: 15}}>
+		To receive money from someone, copy and send the address below or scan this QR code.
+	      </Text>
+	    </View>	    
+	);
+    }
+
+    _renderBalanceRow() {
+	return (
+	    <TouchableOpacity onPress={() => this.setState({balanceHidden: !this.state.balanceHidden})}
+	      style={{
+		      flexDirection: 'row',
+		      justifyContent: 'center',
+		      marginTop: 50
+		  }}>
+
+	      { this.state.balanceHidden ? 
+		  this._renderBalanceButton() :
+		  this._renderBalanceDigit()
+	      }
+	    </TouchableOpacity>
+	)
+    }
+
+    _renderBalanceButton() {
+	return (
+	      <Text style={{ color: '#232836', borderColor: "#232836", borderWidth: 1, padding: 10, fontSize: 20, fontWeight: 'bold' }}>View balance</Text>
+	);
+    }
+
+    _renderBalanceDigit() {
+	const balance  = formatToCurrency(this.props.totalBalance, this.props.currency);	
+	return (
+	    <Text style={{ color: '#02BF19', padding: 10, fontSize: 20, fontWeight: 'bold' }}>{balance}</Text>
+	)
+    }
     
     _renderContent() {
 	const component = this;
@@ -65,12 +116,16 @@ class ReceiveScreen extends React.Component {
 	    <View style={{
 		flex: 1,
 		backgroundColor: '#fff'
-	    }}>
+		  }}>
+	      { this._renderInfo() }
+	      
 		<View style={{
 		    flexDirection: 'row',
 		    justifyContent: 'center',
 		    marginTop: 100
-		}}>
+		      }}>
+
+		  		  
 		  <QRCode
 		     value={ `ethereum:${address}`}
 		     bgColor='#02BF19'
@@ -82,7 +137,9 @@ class ReceiveScreen extends React.Component {
 		    <Text style={{ color: '#02BF19', borderColor: "#02BF19", borderWidth: 1, padding: 5, fontSize: 20, fontWeight: 'bold', marginLeft: 10 }}>Copy</Text>
 		  </View>
 	    </TouchableOpacity>
-		  
+
+
+		{ this._renderBalanceRow() }
 	    
 	    </View>
 	);
@@ -91,7 +148,9 @@ class ReceiveScreen extends React.Component {
 
 
 const mapStateToProps = (state) => ({
-    wallet: getActiveWallet(state)
+    wallet: getActiveWallet(state),
+    totalBalance: getActiveWalletTotalBalance(state),
+    currency: getSelectedCurrency(state),    
 });
 
 
