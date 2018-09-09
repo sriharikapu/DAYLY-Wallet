700aaa852e5b331df70a7ddc111de30bc420c250
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2018-09-09 11:59:12 +0200

    removing extra screens

--- app/views/screens/home/wallet/settings/SettingsScreen.jsx
+++ app/views/screens/home/wallet/settings/SettingsScreen.jsx
@@ -13,20 +13,9 @@ class SettingsScreen extends React.Component {
 		  }}>
 	      <View style={{flex: 4}}>
 		<View style={{marginTop: 40}}>
-		  <SettingsRow navigator={navigator} title="Display Currencies" screen="CurrenciesSettingsScreen" icon={require('quid-wallet/app/views/assets/icons/settings/icon_currency_settings.png')}/>
 		</View>
-		<View style={{marginTop: 20}}>
-		  <SettingsRow navigator={navigator} title="Token List" screen="TokensListScreen" icon={require('quid-wallet/app/views/assets/icons/settings/icon_token_settings.png')}/>
-		</View>	
-		<View style={{marginTop: 20}}>
-		  <SettingsRow navigator={navigator} title="Notifications" screen="NotificationSettingsScreen" icon={require('quid-wallet/app/views/assets/icons/settings/icon_token_settings.png')}/>
-		</View>
-		
 		<View style={{marginTop: 20}}>
 		  <SettingsRow navigator={navigator} title="Unlink Wallet" screen="UnlinkWalletScreen" icon={require('quid-wallet/app/views/assets/icons/settings/icon_unlink_wallet_settings.png')}/>
-		    </View>
-		<View style={{marginTop: 20}}>
-		  <SettingsRow navigator={navigator} title="About" screen="AboutScreen" icon={require('quid-wallet/app/views/assets/icons/settings/icon_about_us.png')}/>
 		    </View>
 	      </View>
 
